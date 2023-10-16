c8647a07bed251c5dc47e539bc51c01908331e88
Artem Blodov <mr.samy@rambler.ru>
2023-10-16 20:37:01 +0200

    Flask8 hw2

--- src/channel.py
+++ src/channel.py
@@ -16,7 +16,19 @@ class Channel:
     Дальше все данные будут подтягиваться по API."""
 
 
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
+        self.info = self.youtube.channels().list(
+            part = "snippet, contentDetails, statistics",
+            id = self.channel_id).execute()
+        self.id = self.info["items"][0]["id"]
+        self.title = self.info["items"][0]["snippet"]["title"]
+        self.overview = self.info["items"][0]["snippet"]["description"]
+        self.url = self.info["items"][0]["snippet"]["thumbnails"]["default"]["url"]
+        self.count_subscribe = self.info["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = self.info["items"][0]["statistics"]["videoCount"]
+        self.video_count = self.info["items"][0]["statistics"]["viewCount"]
+
+
 
 
     def print_info(self) -> None:
@@ -25,3 +37,19 @@ class Channel:
     list(part = "snippet, contentDetails, statistics",
     id = self.channel_id).execute(), indent = 2,
     ensure_ascii = False))
+
+    @classmethod
+    def get_service(cls):
+        return cls
+
+
+    def to_json(self, path):
+        with open(path, "w") as file:
+            json.dump(self.info, file)
+
+
+    @property
+
+
+    def channel_id(self):
+        return self.__channel_id

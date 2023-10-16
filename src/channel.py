47be828639d8a52ad2a92c212d503cd9108b0fb3
Artem Blodov <mr.samy@rambler.ru>
2023-10-16 18:32:27 +0200

    Flask8

--- src/channel.py
+++ src/channel.py
@@ -4,17 +4,24 @@ import json
 
 
 class Channel:
-    """Класс для ютуб-канала"""
+    """Класс для ютуб - канала"""
+
+
     api_key: str = os.getenv('API_KEY_YT')
-    youtube = build('youtube', 'v3', developerKey=api_key)
+    youtube = build('youtube', 'v3', developerKey = api_key)
+
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        """Экземпляр инициализируется id канала.
+    Дальше все данные будут подтягиваться по API."""
+
+
         self.channel_id = channel_id
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.youtube.channels().
-                         list(part="snippet, contentDetails, statistics",
-                              id=self.channel_id).execute(), indent=2,
-                         ensure_ascii=False))
+    list(part = "snippet, contentDetails, statistics",
+    id = self.channel_id).execute(), indent = 2,
+    ensure_ascii = False))

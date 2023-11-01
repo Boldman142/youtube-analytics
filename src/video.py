c34ded3c1f0e7efe7f9f8e0f41f2d32e4a220237
Artem Blodov <mr.samy@rambler.ru>
2023-11-01 12:48:38 +0200

    hw6

--- src/video.py
+++ src/video.py
@@ -8,15 +8,27 @@ class Video:
 
     def __init__(self, id_video: str):  # , channel_id: str):
         #      super().__init__(channel_id)
-        self.video_id = id_video
-        self.info = self.youtube.videos().list(
-            part='snippet,statistics,contentDetails,topicDetails',
-            id=self.video_id).execute()
-        self.url = ('https://www.youtube.com/channel/' +
-                    self.info['items'][0]['id'])
-        self.video_title: str = self.info['items'][0]['snippet']['title']
-        self.view_count: int = self.info['items'][0]['statistics']['viewCount']
-        self.like_count: int = self.info['items'][0]['statistics']['likeCount']
+        try:
+            self.info = self.youtube.videos().list(
+                part='snippet,statistics,contentDetails,topicDetails',
+                id=id_video).execute()["items"][0]["kind"]
+        except IndexError:
+            self.info = None
+            self.url = None
+            self.video_title = None
+            self.view_count = None
+            self.like_count = None
+        else:
+            self.info = self.youtube.videos().list(
+                part='snippet,statistics,contentDetails,topicDetails',
+                id=id_video).execute()
+            self.url = ('https://www.youtube.com/channel/' +
+                        self.info['items'][0]['id'])
+            self.video_title: str = self.info['items'][0]['snippet']['title']
+            self.view_count: int = self.info['items'][0]['statistics']['viewCount']
+            self.like_count: int = self.info['items'][0]['statistics']['likeCount']
+        finally:
+            self.video_id = id_video
 
     def __str__(self):
         return self.video_title
